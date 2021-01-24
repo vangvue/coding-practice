8c28c6f365b25b83f022d46ea9c281c6bfae2d9c
vangvue <vylvue@gmail.com>
2021-01-24 12:11:47 -0600

    finish js-practice-problem-4.js

--- practice-problems/js-practice-problem-4.js
+++ practice-problems/js-practice-problem-4.js
@@ -9,3 +9,23 @@
 // On every subsequent Monday, he will put in $1 more than the previous Monday.
 // Given n, return the total amount of money he will have in the
 // Leetcode bank at the end of the nth day.
+
+var totalMoney = function(n) {
+    let dollarAdd = 1;
+    let days = 7;
+    let moneyCount = 0;
+    let week = 1;
+    for (let i = 0; i < n; i++) {
+        if (days !== 0) {
+            moneyCount += dollarAdd;
+            dollarAdd++;
+            days--;
+        } else {
+            week++;
+            dollarAdd = week;
+            days = 7;
+            i--;
+        }
+    }
+    return moneyCount;
+};
