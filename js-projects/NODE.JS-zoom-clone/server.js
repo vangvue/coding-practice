c2a76d3106d219dea448d31c096b53708fd05259
vangvue <vylvue@gmail.com>
2021-02-12 14:24:41 -0600

    finish setup on localhost:3000

--- js-projects/NODE.JS-zoom-clone/server.js
+++ js-projects/NODE.JS-zoom-clone/server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World")
+})
+
+
+
+
+server.listen(3000);
