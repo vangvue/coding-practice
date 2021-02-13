448ab7e720adc9a959953f20b2107d1cc467be2e
vangvue <vylvue@gmail.com>
2021-02-13 11:24:16 -0600

    establish peer to peer connection, another user is able to share video

--- js-projects/NODE.JS-zoom-clone/server.js
+++ js-projects/NODE.JS-zoom-clone/server.js
@@ -3,9 +3,14 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server)
 const { v4: uuidv4 } = require("uuid"); // Generates Unique ID
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use("/peerjs", peerServer);
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`)
 })
@@ -15,8 +20,9 @@ app.get("/:room", (req, res) => {
 })
 
 io.on("connection", socket => {
-    socket.on("join-room", () => {
-        console.log("Hey we have joined the room.")
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
     })
 })
 
