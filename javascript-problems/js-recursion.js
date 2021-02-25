aa7a37c65e8ec06d240ae4e745dc2b942b084e30
vangvue <vylvue@gmail.com>
2021-02-25 12:02:02 -0600

    finish leetcode problem in js-recursion.js

--- javascript-problems/js-recursion.js
+++ javascript-problems/js-recursion.js
@@ -5,8 +5,8 @@
 // from 1 up to the number passed in.
 // Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
 function sumRange(n) {
-  if (n === 1) return 1;
-  return n + sumRange(n - 1);
+    if (n === 1) return 1;
+    return n + sumRange(n - 1);
 }
 console.log(sumRange(10));
 
@@ -15,8 +15,8 @@ console.log(sumRange(10));
 // Write a function called power which takes in a base and an exponent.
 // If the exponent is 0, return 1.
 function power(base, exponent) {
-  if (exponent === 0) return 1;
-  return base * power(base, exponent - 1);
+    if (exponent === 0) return 1;
+    return base * power(base, exponent - 1);
 }
 
 console.log(power(2, 4));
@@ -27,8 +27,8 @@ console.log(power(2, 4));
 // a factorial of a number is the result of that number multiplied by the number before it,
 // and the number before that number, and so on, until you reach 1. The factorial of 1 is just 1.
 function factorial(n) {
-  if (n === 1) return 1;
-  return n * factorial(n - 1);
+    if (n === 1) return 1;
+    return n * factorial(n - 1);
 }
 
 console.log(factorial(5));
@@ -38,9 +38,9 @@ console.log(factorial(5));
 // Write a function called productOfArray which takes in an array
 // of numbers and returns the product of them all
 function product(arr) {
-  let result = arr.pop()
-  if (arr.length === 1) return result * arr[0]
-  return result * product(arr)
+    let result = arr.pop()
+    if (arr.length === 1) return result * arr[0]
+    return result * product(arr)
 }
 
 console.log(product([1, 2, 3, 10, 10]));
@@ -49,3 +49,23 @@ console.log(product([1, 2, 3, 10, 10]));
 // Write a function called contains that searches for a value in a nested object.
 // It returns true if the object contains that value.
 function contains()
+
+// https://leetcode.com/problems/range-sum-of-bst/
+// 938. Range Sum of BST
+
+// Given the root node of a binary search tree, return the sum of values 
+// of all nodes with a value in the range [low, high].
+
+const rangeSumBST = (root, low, high) => {
+    let sum = 0;
+    const traverse = node => {
+        if (!node) return
+        if (node.val >= low && node.val <= high) {
+            sum += node.val
+        }
+        traverse(node.left)
+        traverse(node.right)
+    }
+    traverse(root);
+    return sum
+};
