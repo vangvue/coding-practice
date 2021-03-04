8ca2aa3557ad0a0bcade0c4585bb89de5756bce7
vangvue <vylvue@gmail.com>
2021-03-04 01:14:33 -0600

    optimize py-pp29 to run faster

--- python-practice/py-pp29.py
+++ python-practice/py-pp29.py
@@ -21,3 +21,17 @@ class Solution:
                 return True
             
         return False
+
+# Optimized Version
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = {}
+        for num in nums:
+            if num not in count.keys():
+                count[num] = 1
+            else:
+                count[num] += 1
+                if count[num] > 1:
+                    return True
+                
+        return False
