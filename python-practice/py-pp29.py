ce4310d8f56dfbbb6c02ef1fe6a9fecfca36633e
vangvue <vylvue@gmail.com>
2021-03-04 00:36:24 -0600

    finish py-pp29.py

---
+++ python-practice/py-pp29.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/contains-duplicate/
+# 217. Contains Duplicate
+
+# Given an array of integers, find if the array contains any duplicates.
+
+# Your function should return true if any value appears at least twice in the array, 
+# and it should return false if every element is distinct.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = {}
+        for num in nums:
+            count[num] = 0
+        
+        for num in nums:
+            if num in count:
+                count[num] += 1
+                
+        for key,val in count.items():
+            if val > 1:
+                return True
+            
+        return False
