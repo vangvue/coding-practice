66de03783a8b3249df87c75e47ac7d3434066cb5
vangvue <vylvue@gmail.com>
2021-03-04 02:27:40 -0600

    optimize py-pp11.py code

--- python-practice/py-pp11.py
+++ python-practice/py-pp11.py
@@ -7,20 +7,37 @@
 
 class Solution:
     def reverseOnlyLetters(self, S: str) -> str:
-        letterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         letterStr = ""
         result = ""
         for char in S:
-            if char in letterList:
+            if char.isalpha():
                 letterStr += char
 
         letterIndex = len(letterStr) - 1 # Will use to find the last index and decrement to reverse str
 
         for char in S:
-            if char in letterList:
+            if char.isalpha():
                 result += letterStr[letterIndex]
                 letterIndex -= 1
             else:
                 result += char
 
         return result
+
+# Optimized Version
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        S = list(S)
+        
+        left = 0
+        right = len(S) - 1
+        while left < right:
+            while left < right and not S[left].isalpha():
+                left += 1
+            while left < right and not S[right].isalpha():
+                right -= 1                
+            S[left], S[right] = S[right], S[left]
+            left += 1
+            right -= 1
+            
+        return "".join(S)
