c3dcbc31fa6eff5423dc99daefe80ce783567e48
vangvue <vylvue@gmail.com>
2021-03-26 14:58:29 -0500

    add py-pp50.py

--- python-practice/py-pp49.py
+++ python-practice/py-pp49.py
@@ -1,7 +1,7 @@
-https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
-1437. Check If All 1's Are at Least Length K Places Away
+# https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+# 1437. Check If All 1's Are at Least Length K Places Away
 
-Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
+# Given an array nums of 0s and 1s and an integer k, return True if all 1's are at least k places away from each other, otherwise return False.
 
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
@@ -18,3 +18,5 @@ class Solution:
                 count += 1
                 
         return True
+
+#Different approach
