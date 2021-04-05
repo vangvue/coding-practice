3ca424dbd52ae30fb70a292200e9f4926a38d526
vangvue <vylvue@gmail.com>
2021-04-05 13:02:45 -0400

    fix js-pp33.js to run faster

--- javascript-problems/js-pp33.js
+++ javascript-problems/js-pp33.js
@@ -1,10 +1,6 @@
-https: //leetcode.com/problems/path-crossing/
-    1496. Path Crossing
+// https: //leetcode.com/problems/path-crossing/
+// 1496. Path Crossing
 
-/**
- * @param {string} path
- * @return {boolean}
- */
 var isPathCrossing = function(path) {
     let x = 0;
     let y = 0;
@@ -40,6 +36,40 @@ var isPathCrossing = function(path) {
 
     for (const [key, value] of Object.entries(obj)) {
         if (value > 1) return true
+    }
+
+    return false
+};
+
+// Less code
+var isPathCrossing = function(path) {
+    let x = 0;
+    let y = 0;
+    let coordArr = [
+        [x, y]
+    ];
+    let obj = {}
+
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] == "N") {
+            coordArr.push([x += 1, y]);
+        } else if (path[i] == "S") {
+            coordArr.push([x -= 1, y]);
+        } else if (path[i] == "E") {
+            coordArr.push([x, y += 1]);
+        } else {
+            coordArr.push([x, y -= 1]);
+        }
+    }
+
+    for (let i = 0; i < coordArr.length; i++) {
+        if (coordArr[i] in obj) {
+            obj[coordArr[i]] += 1;
+        } else {
+            obj[coordArr[i]] = 1;
+        }
+
+        if (obj[coordArr[i]] > 1) return true
     }
 
     return false
