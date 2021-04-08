cc02e5c26d9b61f4bc12ec232aa5c193404daf6a
vangvue <vylvue@gmail.com>
2021-04-08 10:38:46 -0400

    finish js-pp39.js

--- javascript-problems/js-pp39.js
+++ javascript-problems/js-pp39.js
@@ -17,3 +17,21 @@ var flipAndInvertImage = function(image) {
 
     return arr
 };
+
+// Faster runtime
+// Runtime: 80 ms, faster than 91.88% of JavaScript online submissions for Flipping an Image.
+// Memory Usage: 40.6 MB, less than 35.36% of JavaScript online submissions for Flipping an Image.
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    for (let i = 0; i < image.length; i++) {
+        image[i] = image[i].reverse()
+        for (let j = 0; j < image[i].length; j++) {
+            if (image[i][j] == 0) image[i][j] = 1
+            else image[i][j] = 0
+        }
+    }
+    return image
+};
