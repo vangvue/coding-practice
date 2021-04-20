11708b617e4360dba0b84730d2b914a7936e8621
vangvue <vylvue@gmail.com>
2021-04-20 11:01:33 -0400

    finish js-pp49.js

---
+++ javascript-problems/js-pp49.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/thousand-separator/
+// 1556. Thousand Separator
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var thousandSeparator = function(n) {
+    n = n.toLocaleString().toString();
+
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === ",") n = n.replace(n[i], ".")
+    }
+
+    return n
+};
